2eae5e28e6792dcdae0812fbaa2654f8eb8f7afe
adityamankar <aditya.mankar94@gmail.com>
2023-10-09 15:38:21 -0700

    added winning condition and payment
    now wins (stake * 3) are added in credit on getting 7

---
+++ Assets/DiceHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceHandler : MonoBehaviour
+{
+    public Dice[] Dices = new Dice[2];
+    void Start()
+    { 
+    }
+
+    public void RollDices()
+    {
+        foreach (Dice obj in Dices)
+        {
+            if (obj != null) // Check if the object is not null
+            {
+                Debug.Log("rolling dices");
+                obj.SetDiceValue(Random.Range(1, 7));
+                obj.ChangeColor();
+                Debug.Log("rolling dices again");
+            }
+        }
+    }
+
+    public int GetGamplayResult()
+    {
+        int totalDiceValue = 0;
+        foreach (Dice dice in Dices)
+        {
+            totalDiceValue += dice.GetDiceValue();
+        }
+
+        return totalDiceValue;
+    }
+}
