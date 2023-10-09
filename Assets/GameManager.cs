cf08eb0aa2e9e5d8dc9509cec38e5d9b0aa48452
adityamankar <aditya.mankar94@gmail.com>
2023-10-09 10:19:07 -0700

    Handler added
    The game manager is not implemented but the credit handler and bet handler are implemented. It is more generic

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,33 +7,50 @@ public class GameManager : MonoBehaviour
     ////enum of game states idle/play/award
 
     //public List<Dice> Dices = new List<Dice>();
-    //public CreditHandler creditHandler;
-    //public BetHandler betHandler;
+    public CreditHandler creditHandler;
+    public BetHandler betHandler;
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
+    // Static reference to the GameManager instance
+    private static GameManager instance;
 
-    ////singleton
-    //public void RollDiceButtonPressed()
-    //{
-    //    //if current state == idle
-    //    /*for both dices:
-    //            rolldice();
-    //    */
-    //    Debug.Log("play handled by game manager");
-    //    creditHandler.TakeCredit(betHandler.GetCurrBet());
-    //    //change state to running
-    //}
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                // If the instance is null, find it in the scene or create a new GameObject for it
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("GameManager");
+                    instance = obj.AddComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+    }
 
-   
+    private void Awake()
+    {
+        // Ensure there is only one GameManager instance in the scene
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
 
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    public void RollDiceButtonPressed()
+    {
+        //if current state == idle
+        /*for both dices:
+                rolldice();
+        */
+        Debug.Log("play handled by game manager");
+        creditHandler.TakeCredit(betHandler.GetCurrBet());
+        //change state to running
+    }
 }
